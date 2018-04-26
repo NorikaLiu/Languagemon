3e5be9e4bc5927a7e760d48b9516424793b30d40
Ri <norika.l.h@gmail.com>
2018-04-25 18:24:52 -1000

    Committing changs
    Description talks about thing

--- Languagemon/desktop/src/org/kalanihighschool/languagemon/desktop/DesktopLauncher.java
+++ Languagemon/desktop/src/org/kalanihighschool/languagemon/desktop/DesktopLauncher.java
@@ -1,12 +1,19 @@
 package org.kalanihighschool.languagemon.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+//import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+//import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import org.kalanihighschool.languagemon.Languagemon;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.foregroundFPS = 60;
+		config.width = SpaceGame.WIDTH;
+		config.height = SpaceGame.HEIGHT;
+		config.resizable = true;
+		config.addIcon("icon_128.png", FileType.Internal);
+		config.addIcon("icon_32.png", FileType.Internal);
+		config.addIcon("icon_16.png", FileType.Internal);
 		new LwjglApplication(new Languagemon(), config);
 	}
 }
